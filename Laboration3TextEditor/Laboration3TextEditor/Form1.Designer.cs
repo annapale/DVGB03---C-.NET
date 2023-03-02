661fe7fc014e4e4040ce1b18b92cdec192feaf80
annapale <98168925+annapale@users.noreply.github.com>
2023-03-02 14:27:43 +0100

    Version3

---
+++ Laboration3TextEditor/Laboration3TextEditor/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Laboration3TextEditor
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripStatusWith = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusWithout = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusWords = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusRows = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonNewFile = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(12, 63);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(776, 322);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSave.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonSave.Location = new System.Drawing.Point(125, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(107, 45);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSaveAs.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveAs.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonSaveAs.Location = new System.Drawing.Point(238, 12);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(107, 45);
+            this.buttonSaveAs.TabIndex = 2;
+            this.buttonSaveAs.Text = "Save As";
+            this.buttonSaveAs.UseVisualStyleBackColor = false;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonOpen.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpen.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonOpen.Location = new System.Drawing.Point(351, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(107, 45);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClear.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonClear.Location = new System.Drawing.Point(464, 12);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(107, 45);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClose.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonClose.Location = new System.Drawing.Point(577, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(107, 45);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.statusStrip1.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusWith,
+            this.toolStripStatusWithout,
+            this.toolStripStatusWords,
+            this.toolStripStatusRows});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 403);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStripStatusWith
+            // 
+            this.toolStripStatusWith.Name = "toolStripStatusWith";
+            this.toolStripStatusWith.Size = new System.Drawing.Size(161, 17);
+            this.toolStripStatusWith.Text = "Characters with Space:";
+            // 
+            // toolStripStatusWithout
+            // 
+            this.toolStripStatusWithout.Name = "toolStripStatusWithout";
+            this.toolStripStatusWithout.Size = new System.Drawing.Size(182, 17);
+            this.toolStripStatusWithout.Text = "Characters without Space:";
+            // 
+            // toolStripStatusWords
+            // 
+            this.toolStripStatusWords.Name = "toolStripStatusWords";
+            this.toolStripStatusWords.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusWords.Text = "Words:";
+            // 
+            // toolStripStatusRows
+            // 
+            this.toolStripStatusRows.Name = "toolStripStatusRows";
+            this.toolStripStatusRows.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusRows.Text = "Rows:";
+            // 
+            // buttonNewFile
+            // 
+            this.buttonNewFile.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonNewFile.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonNewFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonNewFile.Name = "buttonNewFile";
+            this.buttonNewFile.Size = new System.Drawing.Size(107, 45);
+            this.buttonNewFile.TabIndex = 7;
+            this.buttonNewFile.Text = "New";
+            this.buttonNewFile.UseVisualStyleBackColor = false;
+            this.buttonNewFile.Click += new System.EventHandler(this.buttonNewFile_Click);
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.buttonNewFile);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSaveAs);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "TextEditor";
+            this.Text = "TextEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TextEditor_FormClosing);
+            this.Load += new System.EventHandler(this.TextEditor_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextEditor_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextEditor_DragEnter);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusWith;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusWithout;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusWords;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusRows;
+        private System.Windows.Forms.Button buttonNewFile;
+    }
+}
+
---
+++ Laboration3TextEditor/Laboration3TextEditor/Form1.cs
@@ -0,0 +1,381 @@
+﻿//Anna-Maria Paleczek
+//DVGB07
+//Laboration 3
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Laboration3TextEditor
+{
+    public partial class TextEditor : Form
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+
+            this.AllowDrop = true;
+        }
+
+        //variable for saving fileName 
+        string fileLocation = null;
+
+        //check if text is changed 
+        bool newText = false;
+
+      
+        //open file
+        private void readFile()
+        {
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    FileStream inStream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                    StreamReader reader = new StreamReader(inStream);
+                    string fileText = reader.ReadToEnd();
+                    richTextBox.Text = fileText;
+
+                    //saves file Name so the "save" button works
+                    fileLocation = openFileDialog1.FileName;
+
+                    //change title 
+                    this.Text = Path.GetFileName(fileLocation);
+
+                    //no new text even if text in textbox has been changed
+                    newText = false;
+
+                    reader.Dispose();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        //save text as textfile
+        private void writeFile()
+        {
+            try
+            {
+                DialogResult result = saveFileDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    FileStream outStream = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter writer = new StreamWriter(outStream);
+
+                    writer.Write(richTextBox.Text);
+                    fileLocation = saveFileDialog1.FileName;
+
+                    writer.Dispose();
+
+                    //no usaved text
+                    newText = false;
+                    this.Text = Path.GetFileName(fileLocation);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        //save text in the same file
+        private void saveFile()
+        {
+            try
+            {
+                //if no file is currently openend goes to save as method instead
+                if (fileLocation == null)
+                {
+                    writeFile();
+                }
+                else
+                {
+                    StreamWriter writer = new StreamWriter(fileLocation);
+                    writer.Write(richTextBox.Text);
+                    writer.Dispose();
+
+                    //no unsaved text
+                    newText = false;
+                    this.Text = Path.GetFileName(fileLocation);
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);    
+            }
+
+        }
+
+        //open file
+        private void openFile()
+        {
+            try
+            {
+                //if the text has changed
+                if (newText)
+                {
+                    
+                    DialogResult result = MessageBox.Show("Do you want to save changes?", "Save Changes", MessageBoxButtons.YesNoCancel);
+
+                    //saves old file and opens new file
+                    if (result == DialogResult.Yes)
+                    {
+                        saveFile();
+                        readFile();
+                    }
+                    //opens new file 
+                    else if (result == DialogResult.No)
+                    {
+                        readFile();
+                    }
+                    else if (result == DialogResult.Cancel)
+                    {
+                        return;
+                    } 
+                }
+                else
+                {
+                    readFile();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }                          
+        }
+
+        private void TextEditor_Load(object sender, EventArgs e)
+        {
+            this.Text = "Untitled.txt";
+        }
+
+        private void buttonNewFile_Click(object sender, EventArgs e)
+        {
+            if (newText)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save changes?", "Save Changes", MessageBoxButtons.YesNoCancel);
+
+                //saves old file and opens new file
+                if (result == DialogResult.Yes)
+                {
+                    saveFile();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            
+            richTextBox.Text = string.Empty;
+            this.Text = "Untitled.txt";
+            fileLocation = null;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            writeFile();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = string.Empty;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //adds * to title 
+            if (fileLocation == null)
+            {
+                this.Text = "*Untitled.txt";
+            }
+            else
+            {
+                this.Text = "*" + Path.GetFileName(fileLocation);
+            }
+
+            newText = true;
+
+            //updates status bar
+            string text = richTextBox.Text;
+
+            int charCountWSpaces = text.Length;
+            int charCountWOSpaces = text.Replace(" ", "").Length;
+            int wordCount = text.Split(' ', '\n', '\r').Length;
+            int rowCount = richTextBox.Lines.Length;
+
+            toolStripStatusWith.Text = "Characters With Space: " + charCountWSpaces.ToString();
+            toolStripStatusWithout.Text = "Characters Without Space: " + charCountWOSpaces.ToString();
+            toolStripStatusWords.Text = "Words: " + wordCount.ToString();
+            toolStripStatusRows.Text = "Rows: " + rowCount.ToString();
+        }
+
+        private void TextEditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //save prompt
+            if (newText)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save changes?", "Save Changes", MessageBoxButtons.YesNoCancel);
+                
+                if (result == DialogResult.Yes)
+                {
+                    saveFile(); 
+                }
+
+                //don't close if cancel
+                else if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        //Drag&Drop
+        private void TextEditor_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            try
+            {
+                foreach (string file in files)
+                {
+                   
+                    switch (ModifierKeys)
+                    {
+                        //add text to end of textBox
+                        case Keys.Control:
+                            richTextBox.Text += File.ReadAllText(file);
+                            break;
+
+                        //add text to curser position
+                        case Keys.Shift:
+                            int currentPosition = richTextBox.SelectionStart;
+                            richTextBox.SelectedText = File.ReadAllText(file);
+                            break;
+
+                        //if no key is pressed open file
+                        case Keys.None:
+                         //if text got changed
+                             if (newText)
+                             {
+                                //if a file is already open
+                                if (fileLocation != null)
+                                {
+                                    DialogResult result = MessageBox.Show("Do you want to save changes?", "Save Changes", MessageBoxButtons.YesNoCancel);
+                                    //save file and open new file
+                                    if (result == DialogResult.Yes)
+                                    {
+                                        saveFile();
+
+                                        StreamReader reader = new StreamReader(file);
+                                        string fileText = reader.ReadToEnd();
+                                        richTextBox.Text = fileText;
+
+                                        fileLocation = file;
+
+                                        this.Text = Path.GetFileName(fileLocation);
+
+                                        reader.Dispose();
+
+                                        //fix newText = true from the TextChanged-Method
+                                        newText = false;
+                                    }
+                                    //open new file without saving 
+                                    else if (result == DialogResult.No)
+                                    {
+                                        StreamReader reader = new StreamReader(file);
+                                        string fileText = reader.ReadToEnd();
+                                        richTextBox.Text = fileText;
+
+                                        fileLocation = file;
+
+                                        this.Text = Path.GetFileName(fileLocation);
+
+                                        reader.Dispose();
+
+                                        //fix newText = true from the TextChanged-Method
+                                        newText = false;
+                                    }
+                                    //do nothing
+                                    else if (result == DialogResult.Cancel)
+                                    {
+                                        return;
+                                    }
+
+                                }
+
+                                //if no file is open
+                                else
+                                {
+                                    readFile();
+                                }
+                             }
+
+                             //if text wasnt changed open new file 
+                             else
+                             {
+                                StreamReader reader = new StreamReader(file);
+                                string fileText = reader.ReadToEnd();
+                                richTextBox.Text = fileText;
+
+                                fileLocation = file;
+
+                                this.Text = Path.GetFileName(fileLocation);
+
+                                reader.Dispose();
+
+                                //fix newText = true from the TextChanged-Method
+                                newText = false;
+                             }                         
+                             break;
+                        
+                         //do nothing if any other key is pressed
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void TextEditor_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+       
+    }
+}
